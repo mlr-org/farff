84ee75fc8a3082513ffe351c47bb3b4df315f804
Bernd Bischl <bernd_bischl@gmx.net>
2015-08-21 12:52:53 +0200

    begin unit tests

---
+++ tests/testthat/helper.R
@@ -0,0 +1,10 @@
+library(checkmate)
+INST_DIR = file.path("..", "..", "inst", "arffs")
+
+compareRWeka = function(dir, path) {
+  path2 = file.path(dir, path)
+  d1 = readARFF(path2)
+  d2 = RWeka::read.arff(path2)
+  expect_equal(d1, d3)
+}
+
---
+++ tests/testthat/test_quick.R
@@ -0,0 +1,7 @@
+context("quick test with small arffs")
+
+test_that("quick test with small arffs", {
+
+  compareRWeka(INST_DIR, "iris.arff")
+})
+
