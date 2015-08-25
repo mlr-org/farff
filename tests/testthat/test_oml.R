0f8789d870c914583941f7b13300e46309ccd6bc
Bernd Bischl <bernd_bischl@gmx.net>
2015-08-25 14:06:46 +0200

    clean up

--- tests/testthat/test_oml.R
+++ tests/testthat/test_oml.R
@@ -2,7 +2,7 @@ context("long test with many OML data sets")
 
 library(OpenML)
 dchars = listOMLDataSets()
-dchars2 = subset(dchars, status == "active" & NumberOfInstances < 1000 & NumberOfFeatures < 20)
+dchars2 = subset(dchars, status == "active" & NumberOfInstances < 100 & NumberOfFeatures < 10)
 dids = dchars2$did
 
 # FIXME: dat sets which text features and special chars, they are not stored as UTF8 on OML
@@ -10,8 +10,9 @@ dids = setdiff(dids, c(374, 376,  379,  380))
 # FIXME: data set 292 is in sparse format. this is valid but we cannot parse it.
 dids = setdiff(dids, c(292))
 
-for (did in dids) {
-  compareOML(did)
+for (dreader in c("readr", "data.table")) {
+  for (did in dids) {
+    compareOML(did, data.reader = dreader)
+  }
 }
-
 
