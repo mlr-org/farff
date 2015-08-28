104b4b17c2830d184da17ff8cebe4c46ce7c9c61
Bernd Bischl <bernd_bischl@gmx.net>
2015-08-28 18:48:55 +0200

    clean up

--- tests/testthat/test_oml.R
+++ tests/testthat/test_oml.R
@@ -1,27 +1,30 @@
-context("long test with many OML data sets")
+if (identical(Sys.getenv("TRAVIS"), "true")) {
 
-library(OpenML)
-dchars = listOMLDataSets()
-dchars2 = subset(dchars, status == "active" & NumberOfInstances < 2000 & NumberOfFeatures < 40)
-dids = dchars2$did
+  context("long test with many OML data sets")
 
-
-# FIXME: data set 292 is in sparse format. this is valid but we cannot parse it.
-dids = setdiff(dids, c(292))
+  library(OpenML)
+  dchars = listOMLDataSets()
+  dchars2 = subset(dchars, status == "active" & NumberOfInstances < 2000 & NumberOfFeatures < 40)
+  dids = dchars2$did
 
 
-# FIXME: there are quoting issues in data.table here. if we have doubles quotes ", it is unclear
-# how data.table should get them fed into after preproc.
-# - one " does not work, eg if we get a comma after an unescaped " in dquotes
-# - the correct way would be to escape all dquotes in char fields like this : \".
-#   but now data.table produces \\\"
-dids.datatable.broken = c(374, 376,  379,  380)
+  # FIXME: data set 292 is in sparse format. this is valid but we cannot parse it.
+  dids = setdiff(dids, c(292))
 
-for (dreader in c("readr", "data.table")) {
-  for (did in dids) {
-    print(did)
-    if (dreader == "readr" || did %nin% dids.datatable.broken)
-      compareOML(did, data.reader = dreader)
+
+  # FIXME: there are quoting issues in data.table here. if we have doubles quotes ", it is unclear
+  # how data.table should get them fed into after preproc.
+  # - one " does not work, eg if we get a comma after an unescaped " in dquotes
+  # - the correct way would be to escape all dquotes in char fields like this : \".
+  #   but now data.table produces \\\"
+  dids.datatable.broken = c(374, 376,  379,  380)
+
+  for (dreader in c("readr", "data.table")) {
+    for (did in dids) {
+      print(did)
+      if (dreader == "readr" || did %nin% dids.datatable.broken)
+        compareOML(did, data.reader = dreader)
+    }
   }
-}
 
+}
