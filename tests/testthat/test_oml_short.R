7e9caf5490c366c6065c1cade87a68089d75889b
Bernd Bischl <bernd_bischl@gmx.net>
2016-01-07 22:56:27 +0100

    clean up tests

--- tests/testthat/test_oml_short.R
+++ tests/testthat/test_oml_short.R
@@ -1,15 +1,12 @@
-if (identical(Sys.getenv("TRAVIS"), "true")) {
+context("quick test with OML data sets where problems occured before")
 
-  context("quick test with OML data sets where problems occured before")
+library(OpenML)
+dids = c(119)
 
-  library(OpenML)
-  dids = c(119)
-
-  # for (dreader in c("readr", "data.table")) {
-  for (dreader in c("readr")) {
-    for (did in dids) {
-      compareOML(did, data.reader = dreader)
-    }
+# for (dreader in c("readr", "data.table")) {
+for (dreader in c("readr")) {
+  for (did in dids) {
+    compareOML(did, data.reader = dreader)
   }
 }
 
