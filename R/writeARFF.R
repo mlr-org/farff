22a06393cd0612ad4a4ee4c37a10733f709afb21
Jakob Bossek <info@jakobbossek.de>
2016-03-18 15:31:45 +0100

    chunk large data frames and write by appending chunks (a little bit faster)

--- R/writeARFF.R
+++ R/writeARFF.R
@@ -2,7 +2,9 @@
 #'
 #' @description
 #' Internally uses \code{\link{write.table}} and is therefore not much faster
-#' than RWeka's \code{\link[RWeka]{write.arff}}.
+#' than RWeka's \code{\link[RWeka]{write.arff}}. Moreover, for large data
+#' (> 1e6 rows) the date frame is written out in chunks of 1e6 lines to speed
+#' up the write process.
 #'
 #' @note
 #' Logical columns in R are converted to categorical attributes in ARFF
@@ -16,6 +18,10 @@
 #' @param overwrite [\code{logical(1)}]\cr
 #'   Should \code{path} be overwritten if it already exists?
 #'   Default is \code{FALSE}.
+#' @param chunk.size [\code{integer(1)}]\cr
+#'   Large datesets are splitted before writing out to file into chunks of size
+#'   \code{chunk.size}.
+#'   Default is \code{1e6}.
 #' @param relation [\code{character(1)}]\cr
 #'   Name of the relation in the ARFF file.
 #'   Default is to guess it from the object name.
@@ -23,10 +29,12 @@
 #' @export
 writeARFF = function(x, path,
   overwrite = FALSE,
+  chunk.size = 1e6,
   relation = deparse(substitute(x))) {
   assertDataFrame(x, col.names = "unique", min.rows = 1L, min.cols = 1L)
   assertPathForOutput(path, overwrite = overwrite)
   assertFlag(overwrite)
+  chunk.size = asCount(chunk.size)
   assertString(relation)
 
   handle = file(path, "wb")
@@ -73,7 +81,10 @@ writeARFF = function(x, path,
     writeLines(line, handle, sep = eol)
   }
   writeLines("@data", handle)
-  write.table(x, file = handle, row.names = FALSE, col.names = FALSE, na = "?", sep = ",")
+  chunks = BBmisc::chunk(seq(nrow(x)), chunk.size = chunk.size)
+  for (chunk in chunks) {
+    suppressWarnings(write.table(x[chunk, , drop = FALSE], file = handle, row.names = FALSE, col.names = FALSE, na = "?", sep = ","))
+  }
   invisible(NULL)
 }
 
