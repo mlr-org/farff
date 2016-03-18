6afe270886a0ced3992e118a3661e8894df457e8
Jakob Bossek <info@jakobbossek.de>
2016-03-18 10:13:32 +0100

    add tests for dids which caused problems with line breaks

--- tests/testthat/test_oml_short.R
+++ tests/testthat/test_oml_short.R
@@ -1,7 +1,9 @@
 context("quick test with OML data sets where problems occured before")
 
 library(OpenML)
+
 dids = c(119)
+dids = c(dids, c(1028, 1030)) # caused problems with linebreaks (github issue #9)
 
 # for (dreader in c("readr", "data.table")) {
 for (dreader in c("readr")) {
