8221efbbfee96aa9ecb7b71370859f979f6944a4
Bernd Bischl <bernd_bischl@gmx.net>
2018-11-20 17:33:06 +0100

    improve buffer size in preproc code, to ensure that we dont fail for extremely long lines

--- src/preproc_readr.c
+++ src/preproc_readr.c
@@ -69,8 +69,8 @@ SEXP c_rd_preproc(SEXP s_path_in, SEXP s_path_out, SEXP s_data_sect_index) {
   const char* path_in = CHAR(asChar(s_path_in));
   const char* path_out = CHAR(asChar(s_path_out));
   int data_sect_index = asInteger(s_data_sect_index);
-  char line_buf_1[5000000];
-  char line_buf_2[5000000];
+  char line_buf_1[400000];
+  char line_buf_2[400000];
   char* line_p;
   int data_sect_reached = 0;
 
